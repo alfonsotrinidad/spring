f47d81ebb9287bbd022761dd716602a8617e710a
Alfonso Bolaño Arguello <alfonso.bolao@gmail.com>
2020-11-12 14:40:48 -0500

    guardar y leer todos los usuarios

--- src/main/java/com/sebasoft/api/controllers/ususrioController.java
+++ src/main/java/com/sebasoft/api/controllers/ususrioController.java
@@ -1,29 +1,33 @@
 package com.sebasoft.api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
+import com.sebasoft.api.models.usuarioModel;
 import com.sebasoft.api.services.usuarioService;
-
-
 
 @RestController
 @RequestMapping
 public class ususrioController {
-	
+
 	@Autowired
 	usuarioService usuario;
-	
-	
-	
 
-	 @GetMapping("/app")
-	    String usuarios() 
-	    {
-	        return "Bienvenidos";
-	    }
-	
-	
+	@PostMapping("/app/usuarios/save")
+	public usuarioModel save(@RequestBody usuarioModel usuario) {
+		return this.usuario.save(usuario);
+	}
+
+	public String toString() {
+		return usuario.toString();
+	}
+
+	@GetMapping("/app/usuarios")
+	public ArrayList<usuarioModel> usuarios() {
+		return usuario.findAll();
+	}
 }
